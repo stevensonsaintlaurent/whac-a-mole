ab0abbdfc0bcaa2e4d809dc6495c9f6437af516c
St laurent Stevenson <stevensonstlaurent08@gmail.com>
2024-11-11 01:16:04 +0300

    have a lot thing to fix

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitPosition;
+let currentTime = 10;
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -12,5 +14,34 @@ function randomSquare() {
 
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
 }
-randomSquare();
+
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
+function moveMole() {
+  let timerId = null;
+  timerId = setInterval(randomSquare, 1000);
+}
+moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    alert("Game over your final is " + result);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
